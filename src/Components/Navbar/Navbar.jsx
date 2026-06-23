a34cedda1a0d928a72d650560a7340e81f96d702
Alisher Gulomjanov <qirolfasidon@gmail.com>
2026-06-23 09:50:00 +0500

    added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,28 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div>Alisher</div>
-  )
+    <div>
+      <header>
+        <div>
+          <h1>magnifico</h1>
+        </div>
+        <nav className="navbar">
+          <a href="">Products</a>
+          <a href="">Solutions</a>
+          <a href="">Services</a>
+          <a href="">Help Center</a>
+          <a href="">Pricing</a>
+        </nav>
+        <div className="login-container">
+          <h3>Log in</h3>
+          <button className="login">Sign up free</button>
+        </div>
+      </header>
+      <br />
+      <br />
+      <hr />
+    </div>
+  );
 }
 
 export default Navbar;
